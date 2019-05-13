f15c5412640e6689de5dadf8c297158384c5d316
Elias Friedman <efriedman02@gmail.com>
2019-05-13 15:13:39 -0400

    creating cookie on login

---
+++ src/jwtconfig.ts
@@ -0,0 +1,4 @@
+
+export default {
+  jwtSecret: process.env.SERVER_SESSION_SECERET || 'secret'
+}
--- src/routes/login-router.ts
+++ src/routes/login-router.ts
@@ -1,16 +1,25 @@
 import express from 'express'
 import { comparePassword } from '../strategies/authentication-strategy';
 import { users } from '../state';
+import jwt from 'jsonwebtoken';
+import jwtConfig from '../jwtconfig';
 
 const router = express.Router()
 
 router.post('', (req, res) => {
-  const {username, password} = req.body
+  const { username, password } = req.body
   const user = users.find(user => user.username === username)
-  if(user && password && comparePassword(password, user.password)) {
+  if (user && password && comparePassword(password, user.password)) {
     req.session.user = user
-    res.send('You are logged in')
-  } else if(user && password) {
+    const token = jwt.sign({
+      userId: user.userId,
+      username: user.username,
+      role: user.role
+    },
+      jwtConfig.jwtSecret,
+      { expiresIn: '1h' })
+    res.send(token)
+  } else if (user && password) {
     res.status(401)
     res.send('the username and password do not match')
   } else {
